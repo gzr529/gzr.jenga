0169127863978fdad099d9f114e164a5fe635068
gzr529 <71647892+gzr529@users.noreply.github.com>
2022-07-23 03:07:33 -0400

    Commit 3
    Added AOS
    Added Background
    Added (Some) JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,29 @@
-console.log("RTC Connecting to SpiritPower...")
+import "../../node_modules/aos/src/js/aos.js";
+
+console.log("RTC Connecting to SpiritPower...");
+
+var AOS = require('aos');
+AOS.init();
+
+const ogDares = [
+{dare: "Take a Shot (From Your Revolver)", number: 1, spicy: false},
+{dare: "Murder Your Father", number: 2, spicy: false},
+{dare: "Over Throw the Government", number: 3, spicy: false}
+];
+
+document.getElementById("dares").innerHTML = ogDares[0]; 
+
+console.log(ogDares[0]);
+
+/*
+
+const test = [1,56,23,64,75,37];
+
+function numberSorter() {
+    test.sort();
+}
+
+*/
+
+ogDares.sort(function(a, b){return 0.5 - Math.random()});
+
