6a70654de8ba487e844f9fc8ed73ecedb6c450cd
gzr529 <71647892+gzr529@users.noreply.github.com>
2022-09-15 02:13:21 -0400

    Commit 5
    Randomize Button works, output shown in console

--- src/js/index.js
+++ src/js/index.js
@@ -10,41 +10,17 @@ const originalDares = [
 {dare: "Murder Your Father", dareNumber: 2, spicy: false},
 {dare: "Hug Your Mom", dareNumber: 3, spicy: true},
 {dare: "Over Throw the Government", dareNumber: 4, spicy: false},
+{dare: "Hug Your Dad", dareNumber: 5, spicy: true},
 ];
 
-originalDares.forEach(function(originalDareNames) {
-    console.log(originalDareNames.dare); 
-});
-
-originalDares.forEach(function(originalDareNumber) {
-    console.log(originalDareNumber.dareNumber); 
-});
-
-originalDares.forEach(function(originalDareSpice) {
-    console.log(originalDareSpice.spicy); 
-});
-
-const spicyDares = originalDares.filter(game => game.spicy === true);
-
-originalDares.forEach(function(originalDareSpice) {
-    console.log(originalDareSpice.spicy); 
-});
-
-console.log(spicyDares)
-
-const noSpicyDares = originalDares.filter(game => game.spicy === false);
-
-console.log(noSpicyDares);
-
-
-/*
-
-const test = [1,56,23,64,75,37];
-
-function numberSorter() {
-    test.sort();
+function getRandom(){
+    const randomDare = originalDares[Math.floor(Math.random() * originalDares.length)]
+    console.log(randomDare)
+    console.log(Object.keys(randomDare));
 }
 
-*/
+document.getElementById('randomDareButton').addEventListener('click', getRandom)
+
+
 
 
